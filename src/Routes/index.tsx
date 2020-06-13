adf4a92f6a7c3e945cf88a8877a6e01ee6bd7c10
Rennan Prysthon <rennandelcastillo@gmail.com>
2020-06-13 19:32:46 -0300

    configuracoes iniciais de autenticacao

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {
+  useState,
+  useCallback,
+} from 'react';
+import { api } from '../services/api';
 
-const Routes:React.FC = () => (
-  <div>
-    <h1>Login</h1>
-  </div>
-);
+const Routes:React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const logar = useCallback(
+    async () => {
+      try {
+        const response = await api.post('/admin/login', {
+          email,
+          password,
+        });
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }, [password, email],
+  );
+
+  return (
+    <form onSubmit={logar}>
+      <div>
+        <h1>Login</h1>
+      </div>
+      <div>
+        <input type="email" value={email} onChange={(ctx) => setEmail(ctx.target.value)} />
+        <input type="password" value={password} onChange={(ctx) => setPassword(ctx.target.value)} />
+      </div>
+      <div>
+        <input type="button" onClick={logar} value="Entrar" />
+      </div>
+    </form>
+  );
+};
 
 export default Routes;
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
+  headers: {
+    post: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
 });
 
 api.interceptors.request.use(
