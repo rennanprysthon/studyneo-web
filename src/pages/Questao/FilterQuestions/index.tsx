033c38acdee93090018be3ef87430bb959a977d5
Filipe Melo <filipemelo032@gmail.com>
2020-07-12 16:10:27 -0300

    [Feature] Filters /Filtering Questions

--- src/pages/Questao/FilterQuestions/index.tsx
+++ src/pages/Questao/FilterQuestions/index.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Container, Select } from './styles';
+import { State } from '../../../types/globalstate';
+import { Matter } from '../../../types/filters';
+import { Creators } from '../../../redux/ducks/filter';
 
-const FilterQuestions: React.FC = () => (
-  <Container>
-    <Select>
-      <option value="area">Área de conhecimento</option>
-    </Select>
-    <Select>
-      <option value="area">Matéria</option>
-    </Select>
-    <Select>
-      <option value="area">Assunto</option>
-    </Select>
-  </Container>
-);
+const FilterQuestions: React.FC = () => {
+  const [matters, setMatters] = useState<Matter[]>([]);
+  const areas = useSelector((state:State) => state.filter);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(Creators.request());
+  }, [dispatch]);
+  function handleAreaChange(event: React.FormEvent<HTMLSelectElement>) {
+    if (event.currentTarget.innerText === 'area') { return; };
+    const aux = areas.filter((area) => area.id === Number(event.currentTarget.value))[0];
+    setMatters(aux.matters);
+  }
+  return (
+    <Container>
+      <Select onChange={handleAreaChange}>
+        <option value="area">Área de conhecimento</option>
+        {
+          areas.map((area) => (
+            <option value={area.id} key={area.id}>{area.title}</option>
+          ))
+        }
+      </Select>
+      <Select>
+        <option value="matter">Matéria</option>
+        {
+          matters.map((matter) => (
+            <option value={matter.id} key={matter.id}>{matter.title}</option>
+          ))
+        }
+      </Select>
+      <Select>
+        <option value="area">Assunto</option>
+      </Select>
+    </Container>
+  );
+};
 
 export default FilterQuestions;
