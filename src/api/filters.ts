61a9a237bd8f0be67a1fa8df4216df1550d51b99
Filipe Melo <filipemelo032@gmail.com>
2020-07-12 16:06:27 -0300

    [Fix] Filters / Fixed equest Types

--- src/api/filters.ts
+++ src/api/filters.ts
@@ -2,8 +2,8 @@ import { Area } from '../types/filters';
 import api from '.';
 
 const requestFilters = async () => {
-  const response = await api.get<Area>('/areas');
-  return response;
+  const response = await api.get<Area[]>('/areas');
+  return response.data;
 };
 
 const Api = {
