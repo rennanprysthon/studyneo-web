26977443fbb82e1950d0468a4288bef57da88b1b
Filipe Melo <filipemelo032@gmail.com>
2020-07-18 15:57:49 -0300

    [Feature] Questions / Questions page sanitized

--- src/pages/Questao/QuestaoAdd/index.tsx
+++ src/pages/Questao/QuestaoAdd/index.tsx
@@ -1,143 +1,72 @@
-import React, { useState, useEffect, } from 'react';
+import React from 'react';
 
-import { Container, Form, Input, Alternativas, Label, Submit } from './styles';
-
-import FilterQuestions from '../FilterQuestions';
-import { useSelector } from 'react-redux';
-import { State } from '../../../types/globalstate';
 import { Select } from 'semantic-ui-react';
-import api from '../../../api';
-
-
-interface Alternativa {
-  body: string;
-  isCorrect: boolean;
-}
-
-interface Question {
-  enunciado: string;
-  texto_apoio: string;
-  subject_id: number;
-}
-
-const opcoes = [
-  { key: 'A', value: 'A', text: 'A' },
-  { key: 'B', value: 'B', text: 'B' },
-  { key: 'C', value: 'C', text: 'C' },
-  { key: 'D', value: 'D', text: 'D' },
-  { key: 'E', value: 'E', text: 'E' },
-
-]
+import { useSelector } from 'react-redux';
+import { FiCamera, FiAlignLeft, FiSave } from 'react-icons/fi';
+import {
+  Container, Content, Form, FieldSet, Input, Alternativas, Label, Submit, TextArea, Button,
+} from './styles';
+import FilterQuestions from '../FilterQuestions';
+import { State } from '../../../types/globalstate';
 
 const QuestaoAdd: React.FC = () => {
-  const subjects = useSelector((state: State) => state.subject.selected_subject_id);
-  const [correta, setCorreta] = useState('A');
-
-  const [altA, setAltA] = useState('');
-  const [altB, setAltB] = useState('');
-  const [altC, setAltC] = useState('');
-  const [altD, setAltD] = useState('');
-  const [altE, setAltE] = useState('');
-
-  const [enunciado, setEnunciado] = useState('');
-  const [apoio, setApoio] = useState('');
-
-  const [question, setQuestion] = useState<Question>(() => (
-    {
-      enunciado: '',
-      subject_id: subjects,
-
-    } as Question));
-
-  useEffect(() => {
-    setQuestion(question => ({ ...question, subject_id: subjects, enunciado: enunciado, texto_apoio: apoio }))
-  }, [subjects, enunciado, apoio, correta, altA, altB, altC, altD, altE])
-
-  const submitForm = async () => {
-
-    var alternatives: Alternativa[] = [
-      {
-        body: altA,
-        isCorrect: correta === 'A',
-      },
-      {
-        body: altB,
-        isCorrect: correta === 'B',
-      },
-      {
-        body: altC,
-        isCorrect: correta === 'C',
-      },
-      {
-        body: altD,
-        isCorrect: correta === 'D',
-      },
-      {
-        body: altE,
-        isCorrect: correta === 'E',
-      },
-    ];
-
-    setQuestion(question => ({ ...question, alternatives: alternatives }))
-
-    try {
-      console.log(alternatives)
-      console.log({ ...question, alternatives })
-      const response = await api.post('/questions', { ...question, alternatives });
-      console.log(response)
-    } catch (error) {
-      console.log(error.response)
-    }
-  }
-
+  const subject_id = useSelector((state:State) => state.subject.selected_subject_id);
   return (
     <Container>
-      <Form>
-        <Label>
-          Enunciado
-        </Label>
-        <Input value={question.enunciado} onChange={ctx => setEnunciado(ctx.target.value)} />
-        <Label>
-          Texto Apoio
-        </Label>
-        <Input value={question.texto_apoio} onChange={ctx => setApoio(ctx.target.value)} />
-        <Label>
-          Assunto
-        </Label>
-        <FilterQuestions />
-        <Label>
-          Alternativas
-        </Label>
-        <Alternativas>
+      <Content>
+        <Form>
           <Label>
-            Alternativa A
+            Enunciado
           </Label>
-          <Input value={altA} onChange={ctx => setAltA(ctx.target.value)} />
-          <Label>
-            Alternativa B
-          </Label>
-          <Input value={altB} onChange={ctx => setAltB(ctx.target.value)} />
-          <Label>
-            Alternativa C
-          </Label>
-          <Input value={altC} onChange={ctx => setAltC(ctx.target.value)} />
-          <Label>
-            Alternativa D
-          </Label>
-          <Input value={altD} onChange={ctx => setAltD(ctx.target.value)} />
-          <Label>
-            Alternativa E
-          </Label>
-          <Input value={altE} onChange={ctx => setAltE(ctx.target.value)} />
-        </Alternativas>
-        <Label>
-          Alternativa correta
-          </Label>
-        <Select onChange={(teste, { value }) => setCorreta(`${value}`)} placeholder='Selecione a alternativa correta' options={opcoes} />
-      </Form>
-      <Submit onClick={submitForm}>
-        Finalizar
-      </Submit>
+          <TextArea />
+          <FieldSet>
+            <Button>
+              <FiAlignLeft size={17} />
+              Adicionar Texto de apoio
+            </Button>
+            <Button>
+              <FiCamera size={17} />
+              Adicionar Imagens
+            </Button>
+          </FieldSet>
+          <FieldSet>
+            <Label>
+              A
+            </Label>
+            <Input />
+          </FieldSet>
+          <FieldSet>
+            <Label>
+              B
+            </Label>
+            <Input />
+          </FieldSet>
+          <FieldSet>
+            <Label>
+              C
+            </Label>
+            <Input />
+          </FieldSet>
+          <FieldSet>
+            <Label>
+              D
+            </Label>
+            <Input />
+          </FieldSet>
+          <FieldSet>
+            <Label>
+              E
+            </Label>
+            <Input />
+          </FieldSet>
+          <FilterQuestions />
+          <Submit>
+            <FiSave size={20} />
+            Adicionar questão
+          </Submit>
+        </Form>
+      </Content>
+
     </Container>
   );
 };
--- src/pages/Questao/QuestaoAdd/styles.ts
+++ src/pages/Questao/QuestaoAdd/styles.ts
@@ -2,51 +2,90 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: #fff;
-
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
-
+  align-items: center;
+  justify-content: center;
   * {
     margin: 5px;
   }
 `;
-
+export const Content = styled.div`
+  width: 90%;
+  display: flex;
+  max-width: 700px;
+  padding: 30px;
+`;
 export const Form = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
 `;
 
 export const Input = styled.input`
   background-color: #F0f0f0;
   padding: 10px 15px;
-
+  width: 100%;
+  border-radius: 8px;
 `;
-
+export const TextArea = styled.textarea`
+  background-color: #F0f0f0;
+  padding: 20px;
+  min-height: 150px;
+  width: 100%;
+  resize: none;
+  border-radius: 8px;
+  font-family: 'Rubik', sans-serif;
+`;
 export const Alternativas = styled.div`
   display: flex;
   flex-direction: column;
   padding: 10px;
 `;
 
-export const Label = styled.h5`
+export const Label = styled.h3`
 
 `;
-
-export const Submit = styled.div`
+export const FieldSet = styled.div`
+  display:flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+export const Button = styled.button`
+  padding: 10px;
+  width: 100%;
+  border-radius: 8px;
+  color: #3d3d3d;
+  background: #FFC600;
+  font-weight: bold;
+  transition: all 200ms ease;
+  display:flex;
+  align-items:center;
+  justify-content: center;
   cursor: pointer;
-  padding: 15px 10px;
-  width: 100px;
-  height: 50px;
-  color: #fff;
-  font-weight: bolder;
-  text-align: center;
-  background-color: var(--primary);
-  &:hover {
-    opacity: 0.8;
+  :hover{
+    background:#eeb900;
   }
-  &:active {
-    opacity: 0.3;
+`;
+export const Submit = styled.button`
+  padding: 10px;
+  width: 100%;
+  border-radius: 8px;
+  background: #00B5E2;
+  color: #fff;
+  font-weight: bold;
+  transition: all 500ms ease;
+  display:flex;
+  align-items:center;
+  justify-content: center;
+  cursor: pointer;
+  :hover{
+    background:#eeb900;
+
+  color: #3d3d3d;
   }
 `;
